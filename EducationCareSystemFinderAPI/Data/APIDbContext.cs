6fa5c75a2560b4cde147b0d2ad4e268c87b6b52b
Sajidur <sajidurbd@gmail.com>
2024-06-14 22:20:15 +0200

    Add project files.

---
+++ EducationCareSystemFinderAPI/Data/APIDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EducationCareSystemFinderAPI.Model.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducationCareSystemFinderAPI.Data
+{
+    public class APIDbContext:IdentityDbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options) : base(options)
+        {
+
+        }
+
+       public DbSet<ApplicationUserModel> ApplicationUserModel { get; set; }
+    }
+}
---
+++ EducationCareSystemFinderAPI/Model/Entity/ApplicationUserModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EducationCareSystemFinderAPI.Model.Entity
+{
+    public class ApplicationUserModel:IdentityUser
+    {
+
+    }
+}
